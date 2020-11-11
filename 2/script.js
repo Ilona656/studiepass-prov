6949d312eab2e9eb07258f51f86f3c36cc49a124
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-11-11 14:00:12 +0100

    solve2

--- 2/script.js
+++ 2/script.js
@@ -1,12 +1,13 @@
 
 // 1. Varför loggas ingenting ut när man kör det här skriptet? 
-// Det finns två fel. SVAR: ________
+// Det finns två fel. SVAR: för det är inge string,för att vi inte anropade den
 
 // 2. Ändra scriptet så att "Hello, how are you today?" verkligen loggas ut.
 
-function hello() {
-    console.log(Hello, how are you today?)
+function hello(name = 'Ilona') {
+    console.log('Hello '+ name + ' how are you today?')
 }
+hello()
 
 // 3. Uppdatera funktionen hello så att den tar ett namn som argument
 // Gör så att den hälsar med namn, t.ex. "Hello Fredrik, how are you today?"
