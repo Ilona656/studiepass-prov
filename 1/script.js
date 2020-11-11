169222bfa9f28cbd241b2098730e960537923a0a
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-11-11 13:34:46 +0100

    solve1

--- 1/script.js
+++ 1/script.js
@@ -1,25 +1,30 @@
 
 // 1. Deklarera en variabel a och tilldela den värdet 5. Byt sedan värdet på a till 8
 
+let a = 5
+let a = 8
+
 // 2. Deklarera en konstant variabel tilldela den värdet av ditt förnamn
 
+const name = 'Ilona'
+
 // 3. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: _0___
+// b) Vilken datatyp returnerar den? SVAR: __string___
 function foo() {
     return "foo"
 }
 
 // 4. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: __1__
+// b) Vilken datatyp returnerar den? SVAR: __any___
 function bar(arr) {
     return arr
 }
 
 // 5.
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: __3__
+// b) Vilken datatyp returnerar den? SVAR: __variabel___
 function bar(a, b, c) {
     console.log(a)
 }
